96143e4fed8117c6acd6ca923c687bbb4e1c214e
mahcopration <129929195+mahcopration@users.noreply.github.com>
2023-11-17 22:40:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,7 @@
-python3 -m VideoPlayerBot
+echo "Cloning Repo, Please Wait..."
+git clone -b alpha https://github.com/S1-BOTS/VideoPlayerBot.git /VideoPlayerBot
+cd /VideoPlayerBot
+echo "Installing Requirements..."
+pip3 install -U -r requirements.txt
+echo "Starting Bot, Please Wait..."
+python3 main.py
