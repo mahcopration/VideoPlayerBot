ad821ef82a395a929ba492d0cc3ab8b7169ce170
mahcopration <129929195+mahcopration@users.noreply.github.com>
2023-11-17 22:28:40 +0530

    Update start

--- start.sh
+++ start.sh
@@ -1,7 +1,1 @@
-echo "Cloning Repo, Please Wait..."
-git clone -b alpha https://github.com/S1-BOTS/VideoPlayerBot.git /VideoPlayerBot
-cd /VideoPlayerBot
-echo "Installing Requirements..."
-pip3 install -U -r requirements.txt
-echo "Starting Bot, Please Wait..."
-python3 main.py
+python3 -m DAXXMUSIC
